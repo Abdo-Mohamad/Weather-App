acc5f7f6e22d699d7541effb6c1c37d66d17fb3a
Abdo-Mohamad <abdomohamadwalid@gmail.com>
2022-08-22 00:07:23 +0200

    Display all the element into the  page

--- script.js
+++ script.js
@@ -1,4 +1,4 @@
-// This is function api json and desplay Weather 
+// This is function api json and desplay Weather
 
 let weather = {
   apiKey: "94b923a3ba6e6a26329e3136f2d21e7f",
@@ -7,7 +7,23 @@ let weather = {
       `https://api.openweathermap.org/data/2.5/weather?q=${city}&units=metric&appid=${this.apiKey}`
     )
       .then((response) => response.json())
-      .then((data) => console.log(data));
+      .then((data) => this.displayWeather(data));
   },
-  displayWeather: function (data) {},
+  // Display all the element into the  page 
+  displayWeather: (data) => {
+    const { name } = data;
+    const { icon, description } = data.weather[0];
+    const { temp, humidity } = data.main;
+    const { speed } = data.wind;
+    console.log(name, icon, description, temp, humidity, speed);
+    document.querySelector(".city").innerText = "Weather in " + name;
+    document.querySelector(
+      ".icon"
+    ).src = `https://openweathermap.org/img/wn/${icon}.png`;
+    document.querySelector(".description").innerText = description;
+    document.querySelector(".temp").innerText = temp + "°C";
+    document.querySelector(".humidity").innerText =
+      "Humidity " + humidity + "%";
+    document.querySelector(".wind").innerText = "Wind speed: " + speed + "Km/h";
+  },
 };
