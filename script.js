be6f942f4bda800ef8d48a749d5391021e37084d
Abdo-Mohamad <abdomohamadwalid@gmail.com>
2022-08-23 21:00:58 +0200

    Create a function for getting the user's location with OpenCage Geocoding API

--- script.js
+++ script.js
@@ -36,6 +36,68 @@ let weather = {
     this.fetchWeather(document.querySelector(".search-bar").value);
   },
 };
+// Getting the user's location with OpenCage Geocoding API in javascript
+let geocode = {
+  reverseGeocode: function (latitude, longitude) {
+    var api_key = "c287a425beb74d32b44aea7cc93549e9";
+
+    var api_url = "https://api.opencagedata.com/geocode/v1/json";
+
+    var request_url =
+      api_url +
+      "?" +
+      "key=" +
+      api_key +
+      "&q=" +
+      encodeURIComponent(latitude + "," + longitude) +
+      "&pretty=1" +
+      "&no_annotations=1";
+
+    // see full list of required and optional parameters:
+    // https://opencagedata.com/api#forward
+
+    var request = new XMLHttpRequest();
+    request.open("GET", request_url, true);
+
+    request.onload = function () {
+      // see full list of possible response codes:
+      // https://opencagedata.com/api#codes
+
+      if (request.status === 200) {
+        // Success!
+        var data = JSON.parse(request.responseText);
+        // console.log(data.results[0].components); // print the location
+        weather.fetchWeather(data.results[0].components.state_district);
+      } else if (request.status <= 500) {
+        // We reached our target server, but it returned an error
+
+        console.log("unable to geocode! Response code: " + request.status);
+        var data = JSON.parse(request.responseText);
+        console.log("error msg: " + data.status.message);
+      } else {
+        console.log("server error");
+      }
+    };
+
+    request.onerror = function () {
+      // There was a connection error of some sort
+      console.log("unable to connect to server");
+    };
+
+    request.send(); // make the request
+  },
+  getLocation: function () {
+    function success(data) {
+      geocode.reverseGeocode(data.coords.latitude, data.coords.longitude);
+    }
+    if (navigator.geolocation) {
+      navigator.geolocation.getCurrentPosition(success, console.error);
+    } else {
+      // This a when you reload the page
+      weather.fetchWeather("Barcelona");
+    }
+  },
+};
 
 // This is for the search button
 document.querySelector(".search button").addEventListener("click", function () {
@@ -50,5 +112,32 @@ document
     }
   });
 
-// This a when you reload the page
-weather.fetchWeather("Barcelona");
+geocode.getLocation();
+
+function showLocation(position) {
+  var latitude = position.coords.latitude;
+  var longitude = position.coords.longitude;
+  alert("Latitude : " + latitude + " Longitude: " + longitude);
+}
+
+function errorHandler(err) {
+  if (err.code == 1) {
+    alert("Error: Access is denied!");
+  } else if (err.code == 2) {
+    alert("Error: Position is unavailable!");
+  }
+}
+
+function getLocation() {
+  if (navigator.geolocation) {
+    // timeout at 60000 milliseconds (60 seconds)
+    var options = { timeout: 60000 };
+    navigator.geolocation.getCurrentPosition(
+      showLocation,
+      errorHandler,
+      options
+    );
+  } else {
+    alert("Sorry, browser does not support geolocation!");
+  }
+}
